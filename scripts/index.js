07e1bac247835b706dbd9f4063a0ef6576425c1e
gabrielwinterclark <84819666+gabrielwinterclark@users.noreply.github.com>
2021-06-29 20:22:18 -0400

    Create index.js

---
+++ scripts/index.js
@@ -0,0 +1,25 @@
+let myFlashcards = [ {
+    "question": "Name this species of bird.",
+    "image" : "images/barn-owl-2550068__340.webp",
+    "answer": "Barn Owl"
+    },
+    {
+    "question": "Name this species of bird.",
+    "image" : "images/dove-2516641__340.webp",
+    "answer": "Dove"
+    },
+    {
+    "question": "Name this species of bird.",
+    "image" : "images/hummingbird-2139279__340.webp",
+    "answer": "Hummingbird"
+    }
+  ];
+
+// Make the database point to the location root -> deck -> flashcards
+// If the location doesn't exist is will be created
+firebase.database().ref('deck/flashcards'); 
+ 
+// myFlashcards will be stored under flashcards in the database
+// Anything that was in this location will be overwritten
+// Thus, a write operation also does an update
+firebase.database().set(myFlashcards);
